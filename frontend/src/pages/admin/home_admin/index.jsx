0c888bc1ca6647f506e2b3734f047041a7581e6d
YuriNeves148 <gut.hub.yuri.148@gmail.com>
2026-06-13 11:21:38 -0300

    criada a pagina de "acessar vagas/oportunidades"

--- frontend/src/pages/admin/home_admin/index.jsx
+++ frontend/src/pages/admin/home_admin/index.jsx
@@ -1,6 +1,7 @@
 import "../../../index.css";
 import styles from "./style.module.css";
 import { Navigate, useNavigate, Link } from "react-router-dom";
+import { useState } from "react";
 
 function Home_admin() {
   return (
@@ -33,7 +34,7 @@ function Home_admin() {
             <Link to="/cadastro_professor">cadastrar professor</Link>
           </button>
           <button className={styles.vagas} type="button">
-            <Link to="/vagas_criadas">acessar vagas criadas</Link>
+            <Link to="/vagas_criadas">verificar vagas criadas</Link>
           </button>
         </div>
       </div>
