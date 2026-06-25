5655d472d28aa45ce8e3715a6976709c4dc7aadd
lucas-vasconcelos-pirani-carneiro <lucasvasconcelospiranicarneiro@gmail.com>
2026-06-25 13:13:22 -0300

    fix: correct column name from 'data_nasci' to 'data_nasc' in usuario table

--- database/script.sql
+++ database/script.sql
@@ -76,7 +76,7 @@ CREATE TABLE IF NOT EXISTS usuario (
     matricula VARCHAR(20) UNIQUE NOT NULL,
     nome VARCHAR(100) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
-    data_nasci DATE,
+    data_nasc DATE,
     perfil ENUM('aluno', 'professor', 'admin') NOT NULL DEFAULT 'aluno',
     senha VARCHAR(255) NOT NULL
 );
