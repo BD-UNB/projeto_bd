d5e713a2c146b27123ef354a80929329ecd19cd2
lucas-vasconcelos-pirani-carneiro <lucasvasconcelospiranicarneiro@gmail.com>
2026-06-28 22:04:17 -0300

    feat: update login page to use imported logo and improve accessibility

--- frontend/src/pages/login/index.jsx
+++ frontend/src/pages/login/index.jsx
@@ -2,6 +2,7 @@ import { Link, useNavigate } from "react-router-dom";
 import styles from "./style.module.css";
 import "../../index.css";
 import { useState } from "react";
+import unbLogo from "../../assets/unb_logo.webp";
 
 function Login() {
   const navigate = useNavigate("");
@@ -45,27 +46,27 @@ function Login() {
 
   return (
     <>
-      <div className={styles.header_logo}>
-        <img src="src/assets/unb_logo.webp" alt="Logo da UnB" />
+      <div className = {styles.header_logo}>
+        <img src = {unbLogo} alt="Logo da UnB" />
       </div>
-      <div className={styles.div_boas_vindas}>
+      <div className = {styles.div_boas_vindas}>
         <h1>Bem-vindo ao Portal de Vagas UnB</h1>
         <p>Conectando professores e alunos de forma facilitada</p>
       </div>
-      <div className={styles.container}>
-        <h1 className={styles.h1}>Acesse sua conta</h1>
-        <div className={styles.acesso}>
+      <div className = {styles.container}>
+        <h1 className = {styles.h1}>Acesse sua Conta</h1>
+        <div className = {styles.acesso}>
           <form>
-            <p>Número de Usuário</p>
+            <p>Matrícula</p>
             <input
-              placeholder="Digite o número de usuário"
+              placeholder="Digite a sua Matrícula"
               type="text"
-              value={matricula}
+              value = {matricula}
               onChange={(e) => setMatricula(e.target.value)}
             ></input>
             <p>Senha</p>
             <input
-              placeholder="Digite sua senha"
+              placeholder = "Digite sua Senha"
               type="password"
               value={senha}
               onChange={(e) => setSenha(e.target.value)}
@@ -74,9 +75,6 @@ function Login() {
               Entrar
             </button>
           </form>
-        </div>
-        <div className={styles.cadastro}>
-          <Link to="/home_admin">.</Link>
         </div>
       </div>
     </>
